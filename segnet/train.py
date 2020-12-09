bfd57834d0d3e05c156fb8b0f74e918880717a60
Nima <nazbijar@ucsd.edu>
2020-12-08 23:27:57 -0800

    adjusted architecture and training logic

--- segnet/train.py
+++ segnet/train.py
@@ -1,30 +1,71 @@
 import torch
+from torch.autograd import Variable
 import random
+from torchvision.datasets import Cityscapes
+from torchvision import transforms
 from model import SegNet
+from torch.utils.data import DataLoader
+import wandb
+import sys
 
-N, D_in, H, D_out = 64, 1000, 100, 10
+#wandb.init(project='segnet')
+gpu = False
+if len(sys.argv) > 1 and sys.argv[1] == 'gpu':
+    print("GPU: ", torch.cuda.is_available())
+    gpu = torch.cuda.is_available()
 
-# Create random Tensors to hold inputs and outputs
-x = torch.randn(N,100)
-y = torch.randn(N, 25)
+EPOCHS = 50
+LR = 1e-4
+print("----INIT TRAIN SCRIPT----")
+print("=========================")
+print("Train for {} EPOCHS with learning rate {:.3}".format(EPOCHS, LR))
+#config = wandb.config
+#config.learning_rate = LR
 
-# Construct our model by instantiating the class defined above
-model = SegNet()
 
-# Construct our loss function and an Optimizer. Training this strange model with
-# vanilla stochastic gradient descent is tough, so we use momentum
-criterion = torch.nn.MSELoss(reduction='sum')
-optimizer = torch.optim.SGD(model.parameters(), lr=1e-4, momentum=0.9)
-for t in range(500):
-    # Forward pass: Compute predicted y by passing x to the model
-    y_pred = model(x)
+# Get data and define transformations
+t = transforms.Compose([
+    transforms.Resize((64, 64)),
+    transforms.ToTensor(),
+    ])
 
-    # Compute and print loss
-    loss = criterion(y_pred, y)
-    if t % 100 == 99:
-        print(t, loss.item())
+data = Cityscapes('./data', target_type=['color'], transform=t, target_transform=t)
 
-    # Zero gradients, perform a backward pass, and update the weights.
-    optimizer.zero_grad()
-    loss.backward()
-    optimizer.step()
+train_data = DataLoader(data, batch_size=16, shuffle=True)
+
+# define model
+model = SegNet(64, 64)
+if gpu:
+    model.to(torch.device("cuda:0"))
+
+#wandb.watch(model)
+
+# optimizer and loss definition
+criterion = torch.nn.MSELoss().cuda()
+optimizer = torch.optim.Adam(model.parameters(), lr=LR)
+
+# training
+for epoch in range(EPOCHS):
+    epoch_loss = 0
+    print('-------- EPOCH {} -------'.format(epoch))
+    for i, (inputs, targets) in enumerate(train_data, 0):
+        inputs = inputs.permute(0, 2, 3, 1)
+        
+        if gpu:
+            inputs = inputs.cuda()
+            #targets = targets.cuda()
+
+        y_pred = model(inputs)
+
+        # Compute and print loss
+        loss = criterion(y_pred, inputs)
+        print("BATCH: {} LOSS: {}".format(i, loss.item()))
+        """
+        wandb.log({"epoch": epoch, 
+                   "loss": loss.item(),
+                   })
+        """
+        # Zero gradients, perform a backward pass, and update the weights.
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step
